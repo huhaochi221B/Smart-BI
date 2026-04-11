3d5f0f8307d74cbaa2893b9deb2be5be6d08a15f
huhaochi221B <huhaochi221B@outlook.com>
2026-04-11 12:21:20 +0800

    智能BI项目初始化

---
+++ src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author huhao
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2026-04-11 11:27:26
+* @Entity com.yupi.springbootinit.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author huhao
+* @description 针对表【chart(图表信息表)】的数据库操作Service
+* @createDate 2026-04-11 11:27:26
+*/
+public interface ChartService extends IService<Chart> {
+
+}
